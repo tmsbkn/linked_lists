8ee3276f15fffebce53f5069ece8cfb36db31723
tmsbkn <54986052+tmsbkn@users.noreply.github.com>
2026-07-02 23:43:26 -0400

    Add append and printlist methods to LinkedList class

--- src/modules/LinkedList.js
+++ src/modules/LinkedList.js
@@ -6,5 +6,23 @@ export default class LinkedList {
   }
   append(value) {
     const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = newNode;
+  }
+  printList() {
+    let current = this.head;
+    let result = "";
+    while (current) {
+      result += current.value + "->";
+      current = current.next;
+    }
+    console.log(result + "null");
   }
 }
