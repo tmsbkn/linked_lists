2952c6a15300a039cbee02177a5e317e9180c8fd
tmsbkn <54986052+tmsbkn@users.noreply.github.com>
2026-07-03 09:31:58 -0400

    Update tests in index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import LinkedList from "./modules/LinkedList.js";
 
 const list = new LinkedList();
 
+console.log(list.head(), list.tail());
+
 list.append("Amy");
 list.append("Ruth");
 list.append("Samuel");
 
+list.prepend("Rachel");
 list.printList();
+console.log("Size is:", list.size());
+console.log("The head is", list.head(), "with", list.tail(), "in the tail");
